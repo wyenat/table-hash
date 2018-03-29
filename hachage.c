15974930236acf0d5347f989f9476c0415885f41
Vivien MATTER <Vivien.Matter@grenoble-inp.org>
2018-03-29 13:48:09 +0200

    Réglages de la dernière fois

--- hachage.c
+++ hachage.c
@@ -11,11 +11,11 @@ void liberer(struct annuaire *an) {
 }
 
 
-
+*/
 const char *inserer(struct annuaire *an, const char *nom, const char *numero)
 {
 }
-
+/*
 
 
 const char *rechercher_numero(struct annuaire *an, const char *nom)
--- test.c
+++ test.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 
 int main() {
-  int taille = 10;
-  int a[taille];
-  for (int i=0; i<=10; i++){
-    a[i] = 0;
-    printf("%d \n", a[i]);
-  }
-  printf("%d", a);
+  char taille = 1;
+  int a[] = {1,2,3,4,5};
+  printf("%c \n", a[taille]);
+  printf("%d \n", taille);
 }
